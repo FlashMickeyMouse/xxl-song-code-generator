de3e0f3a2452948b8b9b98f2da17a10d8e049f92
songhao <songhao@lenovo-ar.com>
2019-12-12 09:42:34 +0800

    二开第一次提交

---
+++ xxl-code-generator-admin/src/main/java/com/xxl/codegenerator/admin/Application.java
@@ -0,0 +1,19 @@
+package com.xxl.codegenerator.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuxueli 2018-03-22 23:41:47
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
---
+++ xxl-code-generator-admin/src/main/java/com/xxl/codegenerator/admin/controller/IndexController.java
@@ -0,0 +1,108 @@
+package com.xxl.codegenerator.admin.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.xxl.codegenerator.admin.model.ReturnT;
+import com.xxl.codegenerator.admin.util.FreemarkerUtil;
+import com.xxl.codegenerator.core.CodeGeneratorTool;
+import com.xxl.codegenerator.core.model.ClassInfo;
+import freemarker.template.TemplateException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * sso server (for web)
+ *
+ * @author xuxueli 2017-08-01 21:39:47
+ */
+@Controller
+public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/codeGenerate")
+    @ResponseBody
+    public ReturnT<Map<String, String>> codeGenerate(String tableSql) {
+
+        try {
+
+            if (StringUtils.isBlank(tableSql)) {
+                return new ReturnT<Map<String, String>>(ReturnT.FAIL_CODE, "表结构信息不可为空");
+            }
+
+            // parse table
+            ClassInfo classInfo = CodeGeneratorTool.processTableIntoClassInfo(tableSql);
+
+            // code genarete
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("classInfo", classInfo);
+
+            // result
+            Map<String, String> result = new HashMap<String, String>();
+
+            result.put("controller_code", FreemarkerUtil.processString("controller.ftl", params));
+            result.put("service_code", FreemarkerUtil.processString("service.ftl", params));
+            result.put("service_impl_code", FreemarkerUtil.processString("service_impl.ftl", params));
+
+            result.put("dao_code", FreemarkerUtil.processString("dao.ftl", params));
+            result.put("mybatis_code", FreemarkerUtil.processString("mybatis.ftl", params));
+            result.put("model_code", FreemarkerUtil.processString("model.ftl", params));
+
+            return new ReturnT<Map<String, String>>(result);
+        } catch (IOException | TemplateException e) {
+            logger.error(e.getMessage(), e);
+            return new ReturnT<Map<String, String>>(ReturnT.FAIL_CODE, "表结构解析失败");
+        }
+
+    }
+
+
+    @RequestMapping("/codeGenerateCustom")
+    @ResponseBody
+    public ReturnT<Map<String, String>> codeGenerateCustom(String tableSql,String customftl) {
+
+        try {
+
+            if (StringUtils.isBlank(tableSql)) {
+                return new ReturnT<Map<String, String>>(ReturnT.FAIL_CODE, "表结构信息不可为空");
+            }
+
+
+            if (StringUtils.isBlank(customftl)) {
+                return new ReturnT<Map<String, String>>(ReturnT.FAIL_CODE, "自定义模板信息不可为空");
+            }
+
+            // parse table
+            ClassInfo classInfo = CodeGeneratorTool.processTableIntoClassInfo(tableSql);
+
+            // code genarete
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("classInfo", classInfo);
+
+            // result
+            Map<String, String> result = new HashMap<String, String>();
+
+            System.out.println(JSON.toJSONString(params));
+            result.put("custom_code", FreemarkerUtil.processString("emmmm",customftl, params));
+
+
+            return new ReturnT<Map<String, String>>(result);
+        } catch (IOException | TemplateException e) {
+            logger.error(e.getMessage(), e);
+            return new ReturnT<Map<String, String>>(ReturnT.FAIL_CODE, "表结构解析失败");
+        }
+
+    }
+
+}
